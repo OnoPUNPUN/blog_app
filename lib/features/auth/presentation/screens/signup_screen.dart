8eb50a52d2d7859ad4cb141894727098d783e180
OnoPUNPUN <punpunonodera754@gmail.com>
2026-01-15 23:33:18 +0600

    Initial commit

---
+++ lib/features/auth/presentation/screens/signup_screen.dart
@@ -0,0 +1,74 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignUpScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final TextEditingController _nameTEController = TextEditingController();
+  final GlobalKey<FormState> _fromKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Form(
+          key: _fromKey,
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
+              const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 50, fontWeight: .bold),
+              ),
+              const SizedBox(height: 30),
+              AuthField(hinText: 'Name', controller: _nameTEController),
+              const SizedBox(height: 15),
+              AuthField(hinText: 'Email', controller: _emailTEController),
+              const SizedBox(height: 15),
+              AuthField(
+                hinText: 'Password',
+                controller: _passwordTEController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 20),
+              const AuthGradientButton(),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: "Already Have an Account?",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: ' Sign In',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: AppPallete.gradient1,
+                        fontWeight: .bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _passwordTEController.dispose();
+    _nameTEController.dispose();
+    super.dispose();
+  }
+}
---
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AuthField extends StatelessWidget {
+  final String hinText;
+  final TextEditingController controller;
+  final bool isObscureText;
+  const AuthField({
+    super.key,
+    required this.hinText,
+    required this.controller,
+    this.isObscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(hintText: hinText),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "$hinText is missing";
+        }
+        return null;
+      },
+      obscureText: isObscureText,
+    );
+  }
+}
