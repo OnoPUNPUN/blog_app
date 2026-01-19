682f1c60d653c6da8cb949ac728183ec50d29396
OnoPUNPUN <punpunonodera754@gmail.com>
2026-01-19 13:02:16 +0600

    .

--- lib/features/auth/data/repositorie/auth_repository_impl.dart
+++ lib/features/auth/data/repositorie/auth_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:blog_app/core/error/expectations.dart';
 import 'package:blog_app/core/error/failure.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_datasource.dart';
-import 'package:blog_app/features/auth/domain/entities/user.dart';
+import 'package:blog_app/core/common/entities/user.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 // ignore: implementation_imports
 import 'package:fpdart/src/either.dart';
